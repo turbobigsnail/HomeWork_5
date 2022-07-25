adebe6caaf2581a655cc6fa0e9a49db49bfa005c
PROD <turbobigsnail@mail.ru>
2022-07-25 21:55:50 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,31 @@
-public class Main
-{
-    public  static  void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         //  задача 1
 
-            int[] weight_1 = new int[3];
-            weight_1[0] = 33;
-            weight_1[1] = 77;
-            weight_1[2] = 99;
+        int[] weight_1 = new int[3];
+        weight_1[0] = 33;
+        weight_1[1] = 77;
+        weight_1[2] = 99;
 
+        double[] weight_2 = {1.57, 7.654, 9.986};
 
-            double[] weight_2 = {1.57, 7.654, 9.986};
-
-           char[] weight_3 = {37,37, 34, 36, 36, 36 };
-
+        char[] weight_3 = {37, 37, 34, 36, 36, 36};
 
         //    задача 2
 
-
         {
             for (int i = 0; i < weight_1.length; i++)
-            {
+
                 System.out.print(weight_1[i] + ", ");
-            }
-        }
-        {
-            System.out.println(" ");
+
+            System.out.println(weight_1[weight_1.length - 1]);
         }
         {
             for (int i = 0; i < weight_2.length; i++)
-            {
                 System.out.print(weight_2[i] + ", ");
-            }
-            {
-                System.out.println(" ");
-            }
+
+            System.out.println(weight_2[weight_2.length - 1]);
         }
-
-
         // задача 3
         {
 
@@ -47,40 +35,23 @@ public class Main
 
                 overBack--;
             }
+            System.out.println(weight_3[weight_3.length - 1]);
 
-
-            {
-                System.out.println(" ");
-
-            }
         }
-
-
         // задача 4
-
         {
 
-        for (int x = 0; weight_1.length > x; x++ )
-        {
+            for (int x = 0; weight_1.length > x; x++) {
 
-            if (weight_1[x] % 2 != 0)
+                if (weight_1[x] % 2 != 0)
 
-                weight_1[x]++;
+                    weight_1[x]++;
 
-            {
                 System.out.print(weight_1[x] + " ");
             }
-
         }
-
-
-        }
-
-        }
-
-
-
     }
+}
 
 
 
